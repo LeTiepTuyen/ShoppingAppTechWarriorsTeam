4901a3c913bb5e5b771db1a46f967dfa1b28f552
hongngocdoan <hongngoc2449@gmail.com>
2024-06-09 14:15:21 +0700

    [Task 1] Register And Login Feature

--- app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/LoginRegisterActivity.kt
@@ -3,7 +3,9 @@ package com.example.shoppingapptechwarriorsteam.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shoppingapptechwarriorsteam.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
