bbc9b6fe04bad2c677bcc2c4d7beda849f9673cb
Way-curious <tuyentieple@gmail.com>
2024-06-10 15:27:37 +0700

    feat #2 - Sharing preferences between IntroductionFragment and ViewModel

--- app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/di/AppModule.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/di/AppModule.kt
@@ -1,13 +1,18 @@
 package com.example.shoppingapptechwarriorsteam.activities.di
 
+import android.app.Application
+import com.example.shoppingapptechwarriorsteam.activities.util.Constants.INTRODUCTION_SP
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import android.content.Context.MODE_PRIVATE
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,4 +24,9 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseFirestoreDatabase() = Firebase.firestore
+
+    @Provides
+    fun provideIntroductionSP(
+        application: Application
+    ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
 }
