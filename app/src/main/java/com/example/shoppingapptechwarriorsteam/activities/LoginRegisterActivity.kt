76a5139fc49e56586cf087844dcef17f2974984b
Way-curious <tuyentieple@gmail.com>
2024-06-10 15:27:37 +0700

    feat #2 - Sharing preferences between IntroductionFragment and ViewModel

--- app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/LoginRegisterActivity.kt
@@ -1,7 +1,7 @@
 package com.example.shoppingapptechwarriorsteam.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.shoppingapptechwarriorsteam.R
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/util/Constants.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/activities/util/Constants.kt
@@ -2,4 +2,7 @@ package com.example.shoppingapptechwarriorsteam.activities.util
 
 object Constants {
     const val USER_COLLECTION = "user"
+    const val INTRODUCTION_SP = "introductionSP"
+    const val INTRODUCTION_KEY = "introductionKey"
+
 }
