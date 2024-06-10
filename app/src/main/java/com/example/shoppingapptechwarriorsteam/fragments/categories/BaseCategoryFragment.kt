8aa53c27e8ca741548470fe40d1e4ec40ce3b761
Way-curious <tuyentieple@gmail.com>
2024-06-10 21:35:34 +0700

    feat #4 - adding header and tablayout

---
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/categories/BaseCategoryFragment.kt
@@ -0,0 +1,9 @@
+package com.example.shoppingapptechwarriorsteam.fragments.categories
+
+import androidx.fragment.app.Fragment
+import com.example.shoppingapptechwarriorsteam.R
+
+open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
+
+
+}
---
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/categories/ChairFragment.kt
@@ -0,0 +1,4 @@
+package com.example.shoppingapptechwarriorsteam.fragments.categories
+
+class ChairFragment: BaseCategoryFragment() {
+}
