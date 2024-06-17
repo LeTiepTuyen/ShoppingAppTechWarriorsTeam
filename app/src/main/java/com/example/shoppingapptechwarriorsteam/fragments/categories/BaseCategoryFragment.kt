fb0d55cc4d434f740a2be8b783f1e949783c9cbc
WilliamSimon13 <maithieutin@gmail.com>
2024-06-17 16:16:27 +0700

    feat #7 - update ProductDetailsFragment and all remaining categories

--- app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/categories/BaseCategoryFragment.kt
@@ -1,9 +1,90 @@
 package com.example.shoppingapptechwarriorsteam.fragments.categories
 
+import android.content.AbstractThreadedSyncAdapter
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingapptechwarriorsteam.R
+import com.example.shoppingapptechwarriorsteam.adapters.BestProductsAdapter
+import com.example.shoppingapptechwarriorsteam.databinding.FragmentBaseCategoryBinding
+import com.example.shoppingapptechwarriorsteam.databinding.FragmentMainCategoryBinding
 
 open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
+    private lateinit var binding: FragmentBaseCategoryBinding
+    protected val offerAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
+    protected val bestProductsAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+
+        binding.rvOfferProducts.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if(!recyclerView.canScrollVertically(1) && dx != 0){
+                    onOfferPagingRequest()
+                }
+            }
+        })
+        binding.nestedScrollBaseCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener{ v, _, scrollY, _, _ ->
+            if(v.getChildAt(0).bottom <= v.height + scrollY){
+                onBestOfferPagingRequest()
+            }
+        })
+    }
+
+    fun showOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.GONE
+
+    }
+    fun showBestProductLoading(){
+        binding.bestProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideBestProductLoading(){
+        binding.bestProductsProgressBar.visibility = View.GONE
+    }
+    open fun onOfferPagingRequest(){
+
+    }
+
+    open fun onBestOfferPagingRequest(){
+
+    }
+    private fun setupBestProductsRv(){
+        binding.rvBestProducts.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
+    private fun setupOfferRv(){
+        binding.rvOfferProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+    }
 
 }
--- app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/categories/TableFragment.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/categories/TableFragment.kt
@@ -1,4 +1,80 @@
 package com.example.shoppingapptechwarriorsteam.fragments.categories
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.shoppingapptechwarriorsteam.data.Category
+import com.example.shoppingapptechwarriorsteam.util.Resource
+import com.example.shoppingapptechwarriorsteam.viewmodel.CategoryViewModel
+import com.example.shoppingapptechwarriorsteam.viewmodel.factory.BaseCategoryViewModelFactoryFactory
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
 class TableFragment: BaseCategoryFragment() {
+
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+
+    val viewModel by viewModels<CategoryViewModel> {
+        BaseCategoryViewModelFactoryFactory(firestore, Category.Table)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.offerProducts.collectLatest {
+                when (it){
+                    is Resource.Loading -> {
+                        showOfferLoading()
+                    }
+                    is Resource.Success -> {
+                        offerAdapter.differ.submitList(it.data)
+                        hideOfferLoading()
+                    }
+                    is Resource.Error -> {
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                        hideOfferLoading()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.bestProducts.collectLatest {
+                when (it){
+                    is Resource.Loading -> {
+                        showBestProductLoading()
+                    }
+                    is Resource.Success -> {
+                        bestProductsAdapter.differ.submitList(it.data)
+                        hideBestProductLoading()
+                    }
+                    is Resource.Error -> {
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                        hideBestProductLoading()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+    }
+
+    override fun onBestOfferPagingRequest() {
+
+    }
+
+    override fun onOfferPagingRequest() {
+
+    }
+
 }
