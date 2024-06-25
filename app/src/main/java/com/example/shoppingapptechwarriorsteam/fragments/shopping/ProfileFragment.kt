0ce44ee08acd924a4a38e2cb9d2101bb9d897ce5
hongngocdoan <hongngoc2449@gmail.com>
2024-06-25 14:38:52 +0700

    [Task 4] Updating Product Information Details View and Cart Products View

--- app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/shoppingapptechwarriorsteam/fragments/shopping/ProfileFragment.kt
@@ -1,7 +1,97 @@
 package com.example.shoppingapptechwarriorsteam.fragments.shopping
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.shoppingapptechwarriorsteam.R
+import com.example.shoppingapptechwarriorsteam.activities.LoginRegisterActivity
+import com.example.shoppingapptechwarriorsteam.databinding.FragmentProfileBinding
+import com.example.shoppingapptechwarriorsteam.util.Resource
+import com.example.shoppingapptechwarriorsteam.util.showBottomNavigationView
+import com.example.shoppingapptechwarriorsteam.viewmodel.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import de.hdodenhof.circleimageview.BuildConfig
+import kotlinx.coroutines.flow.collectLatest
 
-class ProfileFragment: Fragment(R.layout.fragment_profile){
+@AndroidEntryPoint
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    val viewModel by viewModels<ProfileViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.constraintProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_userAccountFragment)
+            lifecycleScope.launchWhenStarted {
+                viewModel.user.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            binding.progressbarSettings.visibility = View.VISIBLE
+                        }
+                        is Resource.Success -> {
+                            binding.progressbarSettings.visibility = View.GONE
+                            Glide.with(requireView()).load(it.data!!.imagePath).error(ColorDrawable(
+                                Color.BLACK)).into(binding.imageUser)
+                            binding.tvUserName.text = "${it.data.firstName} ${it.data.lastName}"
+                        }
+                        is Resource.Error -> {
+                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                            binding.progressbarSettings.visibility = View.GONE
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+
+        binding.linearAllOrders.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_allOrdersFragment)
+        }
+
+        binding.linearBilling.setOnClickListener {
+            val action = ProfileFragmentDirections.actionProfileFragmentToBillingFragment(
+                0f,
+                emptyArray(),
+                false
+            )
+            findNavController().navigate(action)
+        }
+
+        binding.linearLogOut.setOnClickListener {
+            viewModel.logout()
+            val intent = Intent(requireActivity(), LoginRegisterActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        binding.tvVersion.text = "Version ${BuildConfig.VERSION_CODE}"
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showBottomNavigationView()
+    }
 }
